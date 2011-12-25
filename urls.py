432c8c18819b3a9994e2d9acb37c48052dafda41
Luis Pedro Coelho <luis@luispedro.org>
2011-12-25 22:57:46 +0100

    STRT Basic infrastructure

---
+++ urls.py
@@ -0,0 +1,13 @@
+import settings
+from django.conf.urls.defaults import *
+from django.contrib import admin
+import gitcms.pages.urls
+import gitcms.files.urls
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^media/(?P<path>.+)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    (r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += gitcms.files.urls.urlpatterns
+urlpatterns += gitcms.pages.urls.urlpatterns

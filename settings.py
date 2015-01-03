9c4fab811c10a609e41583159e5a3acf61c12c42
Luis Pedro Coelho <luis@luispedro.org>
2015-01-03 11:47:59 +0100

    ENH Update for newer django

--- settings.py
+++ settings.py
@@ -39,20 +39,20 @@ USE_I18N = True
 STATIC_ROOT = _BASE_DIR + '/static/'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = STATIC_ROOT
+MEDIA_ROOT = _BASE_DIR + '/media/'
 MEDIA_URL = STATIC_URL
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin-media/'
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ezuzpPwnve#2acdaezcizxasxaxkas@#jalskj#$laxlkjasvb'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-    'django.template.loaders.eggs.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
 )
+
+
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -84,7 +84,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-    'django.contrib.markup',
+    # 'django.contrib.markup',
 
     'compressor',
 
